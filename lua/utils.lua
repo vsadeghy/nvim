8148d5767fa01c383f4c5dcc0409a63114a784ef
vsadeghy <v.sadeghy@gmail.com>
2022-01-08 18:15:09 +0330

    added utils

---
+++ lua/utils.lua
@@ -0,0 +1,29 @@
+local function map(mode, key, command, opts, bufnr)
+  local options = { noremap = true, silent = true }
+  options = vim.tbl_extend("force", options, opts or {})
+  if bufnr then
+    vim.api.nvim_buf_set_keymap(bufnr, mode, key, command, options)
+  else
+    vim.api.nvim_set_keymap(mode, key, command, options)
+  end
+end
+
+function nmap(key, command, options)
+  map("n", key, command, options)
+end
+
+function imap(key, command, options)
+  map("i", key, command, options)
+end
+
+function vmap(key, command, options)
+  map("v", key, command, options)
+end
+
+function cmap(key, command, options)
+  map("c", key, command, { silent = false })
+end
+
+function bnmap(bufnr, key, command, options)
+  map("n", key, command, options, bufnr)
+end
