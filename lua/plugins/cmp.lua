dba6a8fd28286b6e0d8ac1246b193e5687470e50
GitExample <git@example.com>
2022-01-18 05:08:23 +0330

    refactored cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,17 +8,118 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
+local cmpconf = {
+  kind_icons = {
+    Text = " ",
+    Function = " ",
+    Method = " ",
+    Constructor = " ",
+    Field = " ",
+    Variable = " ",
+    Class = " ",
+    Interface = " ",
+    Module = " ",
+    Property = " ",
+    Unit = " ",
+    Value = " ",
+    Enum = " ",
+    Keyword = " ",
+    Snippet = " ",
+    Color = " ",
+    File = " ",
+    Reference = " ",
+    Folder = " ",
+    EnumMember = " ",
+    Constant = " ",
+    Struct = " ",
+    Event = " ",
+    Operator = " ",
+    TypeParameter = " ",
+  },
+  source_names = {
+    nvim_lsp = "(LSP)",
+    emoji = "(Emoji)",
+    path = "(Path)",
+    calc = "(Calc)",
+    cmp_tabnine = "(Tabnine)",
+    vsnip = "(vsnip)",
+    luasnip = "(luasnip)",
+    buffer = "(Buffer)",
+  },
+  duplicates = {
+    buffer = 1,
+    path = 1,
+    nvim_lsp = 0,
+    luasnip = 1,
+  },
+  duplicates_default = 0,
+}
+
+local format = {
+  lspkind = lspkind.cmp_format {
+    with_text = false,
+    maxwidth = 50,
+    before = function(entry, vim_item)
+      vim_item.menu = cmpconf.source_names[entry.source.name]
+      vim_item.dup = cmpconf.duplicates[entry.source.name] or cmpconf.duplicates_default
+      return vim_item
+    end,
+  },
+  normal = function(entry, vim_item)
+    vim_item.kind = cmpconf.kind_icons[vim_item.kind]
+    vim_item.menu = cmpconf.source_names[entry.source.name]
+    vim_item.dup = cmpconf.duplicates[entry.source.name] or cmpconf.duplicates_default
+    return vim_item
+  end,
+}
+
 cmp.setup {
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  completion = {
+    keyword_length = 1,
+  },
+  experimental = {
+    ghost_text = true,
+    native_menu = false,
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    kind_icons = cmpconf.kind_icons,
+    source_names = cmpconf.source_names,
+    duplicates = cmpconf.duplicates,
+    duplicates_default = cmpconf.duplicates_default,
+    format = format.normal
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "cmp_tabnine" },
+    { name = "nvim_lua" },
+    { name = "buffer" },
+    { name = "path" },
+    { name = "calc" },
+    { name = "emoji" },
+    { name = "treesitter" },
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
+  },
+  -- documentation = false,
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-e>"] = cmp.mapping {
@@ -50,39 +151,5 @@ cmp.setup {
         fallback()
       end
     end, { "i", "s" }),
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = lspkind.cmp_format {
-      with_text = false, -- do not show text alongside icons
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
-        -- ...
-        return vim_item
-      end,
-    },
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "cmp_tabnine" },
-    { name = "path" },
-    { name = "emoji" },
-  },
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-  -- documentation = false,
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
-  experimental = {
-    ghost_text = true,
-    native_menu = false,
   },
 }
