bc90c975912994414c640c3fc1d159102b55ce98
GitExample <git@example.com>
2022-01-23 22:21:55 +0330

    removed comments

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,21 +1,17 @@
-vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
-vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-vim.g.nvim_tree_group_empty = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
-vim.g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-vim.g.nvim_tree_symlink_arrow = " >> " --  defaults to " ➛ ". used as a separator between symlinks" source and target.
-vim.g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer"s when opening nvim-tree.
-vim.g.nvim_tree_create_in_closed_folder = 0 -- 1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-vim.g.nvim_tree_refresh_wait = 500 -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
--- vim.g.nvim_tree_window_picker_exclude = { "filetype"= { "notify", "packer", "qf" }, "buftype"= { "terminal" } }
---  Dictionary of buffer option names mapped to a list of option values that
---  indicates to the window picker that the buffer"s window should not be
---  selectable.
-vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE" } --  List of filenames that gets highlighted with NvimTreeSpecialFile
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_disable_window_picker = 1
+vim.g.nvim_tree_icon_padding = " "
+vim.g.nvim_tree_symlink_arrow = " >> "
+vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_create_in_closed_folder = 0
+vim.g.nvim_tree_refresh_wait = 500
+vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE" }
 vim.g.nvim_tree_icons = {
   default = "",
   symlink = "",
@@ -23,24 +19,13 @@ vim.g.nvim_tree_icons = {
   folder = { default = "", open = "", empty = "", empty_open = "", symlink = "" },
 }
 
---vim.g.nvim_tree_ignore = {
---    "node_modules",
---    ".git",
---    ".vscode",
---    "dist",
---    "package-lock.json",
---    "yarn.lock",
---}
 --  a list of groups can be found at `:help nvim_tree_highlight`
 vim.cmd "highlight NvimTreeFolderIcon guibg=blue"
---
---
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
+
 require("nvim-tree").setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = true,
+  open_on_setup = false,
   ignore_ft_on_setup = { ".git" },
   auto_close = false,
   open_on_tab = false,
@@ -85,7 +70,7 @@ require("nvim-tree").setup {
     auto_resize = false,
     mappings = {
       custom_only = false,
-      -- list = {{key = "l", cb = require("nvim-tree.config").nvim_tree_callback("edit")}}
+      -- list = list
     },
     number = true,
     relativenumber = true,
@@ -97,8 +82,8 @@ require("nvim-tree").setup {
 }
 
 -- default mappings
+--[[
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
--- stylua: ignore
 local list = {
   { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
   { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
@@ -135,3 +120,4 @@ local list = {
   { key = "q",                            cb = tree_cb("close") },
   { key = "g?",                           cb = tree_cb("toggle_help") },
 }
+]]
