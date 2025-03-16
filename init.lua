d0c5ad6aa510ed905b1607f261ced53b8a6a65ce
vsadeghy <v.sadeghy@gmail.com>
2025-03-16 19:18:39 +0330

    separate load_lazy

--- init.lua
+++ init.lua
@@ -3,28 +3,9 @@ require("options")
 require("keymaps")
 require("autocmds")
 
+require("load_lazy")
+
 local set = require("utils.set")
-
-local load_lazy = set(function()
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable", -- latest stable release
-            lazypath,
-        })
-    end
-    vim.opt.rtp:prepend(lazypath)
-end, function()
-    -- Short URL will be replaced
-    vim.opt.rtp:prepend([[lazy.nvim-plugin-path]])
-end)
-
-load_lazy()
-
 require("lazy").setup("plugins", {
     rocks = { enabled = false },
     performance = { rtp = { reset = set(true, false) } },
--- lua/options.lua
+++ lua/options.lua
@@ -1,56 +1,63 @@
--- Leader key
 vim.g.mapleader = " "
 
--- Very useful options
--- clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- Line numbers
-vim.opt.rnu = true
-vim.opt.nu = true
-vim.opt.scrolloff = 15
-
--- Tabs
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
--- Wrap
-vim.opt.wrap = false
-vim.opt.breakindent = true
-
--- Undo files
-vim.opt.undofile = true
-
--- Searching
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Update time
-vim.opt.updatetime = 100
-vim.opt.timeout = true
-vim.opt.timeoutlen = 300
-
--- Cool colors
-vim.opt.cursorline = true
+vim.wo.number = true -- Set numbered lines (default: false)
+vim.opt.signcolumn = "yes" -- Keep signcolumn on by default (default: "auto")
+vim.opt.relativenumber = true -- Set relative numbered lines (default: false)
+vim.opt.wrap = false -- Display lines as one long line (default: true)
+vim.opt.linebreak = true -- Companion to wrap, don"t split words (default: false)
+vim.opt.mouse = "a" -- Enable mouse mode (default: "")
+vim.opt.autoindent = true -- Copy indent from current line when starting new one (default: true)
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+vim.opt.smartcase = true -- Smart case (default: false)
+vim.opt.shiftwidth = 2 -- The number of spaces inserted for each indentation (default: 8)
+vim.opt.tabstop = 2 -- Insert n spaces for a tab (default: 8)
+vim.opt.softtabstop = 2 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+vim.opt.expandtab = true -- Convert tabs to spaces (default: false)
+vim.opt.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
+vim.opt.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
+vim.opt.cursorline = true -- Highlight the current line (default: false)
 vim.opt.colorcolumn = { 80, 81 }
+vim.opt.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
+vim.opt.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
+vim.opt.hlsearch = false -- Set highlight on search (default: true)
+vim.opt.showmode = false -- We don"t need to see things like -- INSERT -- anymore (default: true)
+vim.opt.whichwrap = "bs<>[]hl" -- Which "horizontal" keys are allowed to travel to prev/next line (default: "b,s")
+vim.opt.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+vim.opt.swapfile = false -- Creates a swapfile (default: true)
+vim.opt.smartindent = true -- Make indenting smarter again (default: false)
+vim.opt.showtabline = 2 -- Always show tabs (default: 1)
+vim.opt.backspace = "indent,eol,start" -- Allow backspace on (default: "indent,eol,start")
+vim.opt.pumheight = 10 -- Pop up menu height (default: 0)
+vim.opt.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+vim.opt.fileencoding = "utf-8" -- The encoding written to a file (default: "utf-8")
+vim.opt.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
+vim.opt.breakindent = true -- Enable break indent (default: false)
+vim.opt.updatetime = 250 -- Decrease update time (default: 4000)
+vim.opt.timeoutlen = 300 -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
+vim.opt.backup = false -- Creates a backup file (default: false)
+vim.opt.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+vim.opt.undofile = true -- Save undo history (default: false)
+vim.opt.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: "menu,preview")
+vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+vim.opt.list = true
+vim.opt.hidden = true
 
--- Required for formatting I think, can't be fucked to check
--- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-
--- Set whitespace characters
+vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
 vim.opt.listchars:append({
     multispace = "·",
     lead = "·",
-    trail = "·",
+    trail = "~",
     nbsp = "·",
     eol = "↵",
+    tab = ">-",
+    extends = ">",
+    precedes = "<",
 })
-vim.opt.list = true
 
+--
 -- local colorschemeName = "catppuccin-mocha"
 -- vim.cmd.colorscheme(colorschemeName)
 
