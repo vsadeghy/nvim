8a344a2fe87486ae5cb078290d32d51e04707e6f
vsadeghy <v.sadeghy@gmail.com>
2025-05-20 03:34:55 +0330

    add markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,20 +1,10 @@
-local set = require "utils.set"
-
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = set(function()
-      vim.fn["mkdp#util#install"]()
-    end),
-  },
   {
     "OXY2DEV/markview.nvim",
     ft = "markdown",
-
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
+    opts = {},
   },
 }
