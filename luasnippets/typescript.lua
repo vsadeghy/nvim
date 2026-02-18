2068e32b8cd8c4a2764fcf3bdbed8c4d48ad3fe3
vsadeghy <v.sadeghy@gmail.com>
2026-02-18 13:18:41 +0330

    add js/ts log snippets

--- luasnippets/typescript.lua
+++ luasnippets/typescript.lua
@@ -5,12 +5,17 @@ for _, ft in ipairs {"typescriptreact","javascript", "javascriptreact"} do
 	ls.filetype_extend(ft, {"typescript"})
 end
 
+local function first(_, x) return x.captures[1] end
+
 return {
+	s({ trig = "([^%s]+)%.L", snippetType = "autosnippet", regTrig = true },
+		fmta([[console.log({ <> });]], { f(first) })
+	),
 	s({ trig = "([^%s]+)%.lg", snippetType = "autosnippet", regTrig = true },
-		fmta([[console.log("<>: ", <>);]], {
-			f(function(_, x) return x.captures[1] end),
-			f(function(_, x) return x.captures[1] end),
-		})
+		fmta([[console.log("<>: ", <>);]], { f(first), f(first) })
+	),
+	s({ trig = "([^%s]+)%.db", snippetType = "autosnippet", regTrig = true },
+		fmta([[console.debug("<>: ", <>);]], { f(first), f(first) })
 	),
 	s({ trig = ";l", snippetType = "autosnippet" }, t "console.log();"),
 	s({ trig = ";i", snippetType = "autosnippet"}, fmta([[import {<>} from "<>";<>]], {
