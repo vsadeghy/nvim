db022817b0b4dabff3fa6d01a6f37975c3081ecb
vsadeghy <v.sadeghy@gmail.com>
2026-06-06 16:34:11 +0330

    update typescript snippets

--- luasnippets/typescript.lua
+++ luasnippets/typescript.lua
@@ -8,16 +8,18 @@ end
 local function first(_, x) return x.captures[1] end
 
 return {
-	s({ trig = "([^%s]+)%.L", snippetType = "autosnippet", regTrig = true },
+	s({ trig = "([^%s]+)%;l", snippetType = "autosnippet", regTrig = true },
 		fmta([[console.log({ <> });]], { f(first) })
 	),
-	s({ trig = "([^%s]+)%.lg", snippetType = "autosnippet", regTrig = true },
+	s({ trig = "([^%s]+)%;g", snippetType = "autosnippet", regTrig = true },
 		fmta([[console.log("<>: ", <>);]], { f(first), f(first) })
 	),
-	s({ trig = "([^%s]+)%.db", snippetType = "autosnippet", regTrig = true },
+	s({ trig = "([^%s]+)%;d", snippetType = "autosnippet", regTrig = true },
 		fmta([[console.debug("<>: ", <>);]], { f(first), f(first) })
 	),
-	s({ trig = ";l", snippetType = "autosnippet" }, t "console.log();"),
+	s({ trig = ";l", snippetType = "autosnippet"}, fmta([[console.log(<>);<>]], {
+		i(1), i(2)
+	})),
 	s({ trig = ";i", snippetType = "autosnippet"}, fmta([[import {<>} from "<>";<>]], {
 		i(2), i(1), i(3)
 	})),
