6ea21895e7b78d35b8e79a29e048e96190a761dd
vsadeghy <v.sadeghy@gmail.com>
2026-07-02 18:46:02 +0330

    add svelte

---
+++ lsp/svelte.lua
@@ -0,0 +1,40 @@
+---@type vim.lsp.Config
+return {
+  cmd = { 'svelteserver', '--stdio' },
+  filetypes = { 'svelte' },
+  settings = {
+    typescript = {
+      inlayHints = {
+        parameterNames = {
+          enabled = 'literals',
+          suppressWhenArgumentMatchesName = true,
+        },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+  },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    if vim.uv.fs_stat(fname) ~= nil then
+      local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.lock' }
+      root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+        or vim.list_extend(root_markers, { '.git' })
+      local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+      on_dir(project_root)
+    end
+  end,
+  on_attach = function(client)
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      pattern = { '*.js', '*.ts' },
+      group = vim.api.nvim_create_augroup('lspconfig.svelte', {}),
+      callback = function(ctx)
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
+      end,
+    })
+  end,
+}
