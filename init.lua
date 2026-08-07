c7384bff54590d30ebc5a21e8094c097bce09015
vsadeghy <v.sadeghy@gmail.com>
2026-08-07 19:14:06 +0330

    move autocmds to their own module

--- init.lua
+++ init.lua
@@ -100,36 +100,7 @@ Map({ "", "!", "t" }, "<M-Down>", move_pane("j", "-D"))
 Map({ "", "!", "t" }, "<M-Left>", move_pane("h", "-L"))
 Map({ "", "!", "t" }, "<M-Right>", move_pane("l", "-R"))
 
---autocmds
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "man",
-	callback = function()
-		Map("n", "d", "<C-d>", { nowait = true })
-		Map("n", "u", "<C-u>", { nowait = true })
-	end,
-})
-vim.api.nvim_create_autocmd("TermOpen", { command = "startinsert" })
-vim.api.nvim_create_autocmd("BufReadPost", { command = 'norm! `"zz' })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function() vim.highlight.on_yank { timeout = 150 } end,
-})
-vim.api.nvim_create_autocmd("FileType", {
-	callback = function() vim.opt.formatoptions:remove { "c", "r", "o" } end,
-})
--- close [No Name] buffers
-vim.api.nvim_create_autocmd("BufReadPost", {
-	callback = function()
-		for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-			if vim.api.nvim_buf_get_name(bufnr) == "" then
-				local chars = 0
-				for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
-					chars = chars + #line
-				end
-				if chars == 0 then vim.api.nvim_buf_delete(bufnr, { force = true }) end
-			end
-		end
-	end,
-})
+require "autocmd"
 
 if vim.version().minor < 12 then return end
 --plugins
